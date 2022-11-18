f931536742de77d1db159ac09298c0f20d298b37
Henry Sullivan <sullivanbhenry@gmail.com>
2022-11-18 08:39:54 -0800

    finish API logic, begin builing business and UI logic

--- src/currency-services.js
+++ src/currency-services.js
@@ -2,7 +2,17 @@ export default class CurrencyService {
   static getExchange(currency, amount) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = 
-    })
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${amount}`;
+      request.addEventListener("loadend", function() {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve([response, currency, amount]);
+        } else {
+          reject([this, response, currency, amount]);
+        }
+      });
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyService from './currency-services';
+
+//Business Logic
+
+
+function getExchange(currency, amount) {
+  let promise = CurrencyService.getExchange(currency, amount);
+  promise.then(function(currencyDataArray) {
+    printElements(currencyDataArray);
+  }, function(errorArray) {
+    printError(errorArray);
+  });
+}
